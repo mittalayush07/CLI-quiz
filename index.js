4c1dd3ea3b1f170c0536d798a0fa4a78d4492656
Ayush Mittal <mittalayush0703@gmail.com>
2024-11-17 10:51:27 +0530

    created the database of questions

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("hello world");
+const input = require('readline');
+/* Creating data set*/
+const database = {
+    data : [
+        {
+            question: `let a = {}, b = {}
+console.log(a==b)
+console.log(a===b)`,
+            options: {
+                a: "false, false",
+                b: "false, true",
+                c: "true, false",
+                d: "true, true"
+            },
+            correctAnswer: "a"
+        },
+        {
+            question: "Object.assign(target, sourse) creates which type of copy?",
+            options: {
+                a: "Deep Copy",
+                b: "Shallow Copy",
+                c: "Nested Copy",
+                d: "Creates a new reference"
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "Is method chaining can be possible with forEach?",
+            options: {
+                a: "Yes",
+                b: "No"
+            },
+            correctAnswer: "b"
+        }
+    ]
+}
+function showQuestionAndOptions(){
+    for(let i=0; i<database.data.length; i++){
+        console.log(`\nQ${i+1} - ${database.data[i].question}`);
+        for(let key in database.data[i].options){
+            console.log(`${key} - ${database.data[i].options[key]}`);
+        }
+        let userName=input.question("Enter your name");
+    }
+}
+showQuestionAndOptions();
+
+
